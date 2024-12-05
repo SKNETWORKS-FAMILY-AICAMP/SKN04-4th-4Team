fc88b47677d8f41683debe345d73fb3298bcd625
JinhyoPark <jinhyo97@gmail.com>
2024-12-05 14:47:25 +0900

    Feat: docker 연결을 위한 settings.py 수정

--- new_employee_assistant/settings.py
+++ new_employee_assistant/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-70vc8wr2^r^85z*+7--!*9r%8$+q0^^38y&k1qffbt8yang2t9"
+SECRET_KEY = os.environ.get('SECRET_KEY', "django-insecure-70vc8wr2^r^85z*+7--!*9r%8$+q0^^38y&k1qffbt8yang2t9")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', True)
 
-ALLOWED_HOSTS = []
+if os.environ.get('DJANGO_ALLOWED_HOSTS'):
+    ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
+else:
+    ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -76,11 +79,14 @@ WSGI_APPLICATION = "new_employee_assistant.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": os.environ.get('SQL_ENGINE', "django.db.backends.sqlite3"),
+        "NAME": os.environ.get('SQL_DATABASE', BASE_DIR / "db.sqlite3"),
+        "USER": os.environ.get('SQL_USER', 'user'),
+        "PASSWORD": os.environ.get('SQL_PASSWORD', 'password'),
+        "HOST": os.environ.get('SQL_HOST', 'localhost'),
+        "PORT": os.environ.get('SQL_PORT', '5432'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
